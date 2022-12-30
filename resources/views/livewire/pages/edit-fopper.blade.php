501d870c56f1d4d4b884e4024e4255eb9eb2dae0
Pascal Masson <pascal220900@gmail.com>
2022-12-30 19:49:32 +0100

    Release

--- resources/views/livewire/pages/edit-fopper.blade.php
+++ resources/views/livewire/pages/edit-fopper.blade.php
@@ -4,7 +4,7 @@
     >
         <div class="mb-6">
             <label for="fopperVoor"
-                   class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Voor:</label>
+                   class="mb-2 block text-sm font-medium text-gray-900 dark:text-gray-300">Voor:</label>
             <select id='fopperVoor' name="fopperVoor" style='width: 200px;' class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" wire:model="fopper.fopperVoor">
                 @foreach($users as $user)
                     <option disabled>Selecteer een naam</option>
