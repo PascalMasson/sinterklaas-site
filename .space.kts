3b0182adfa11799a314f53be127255c6fb954327
Pascal Masson <admin@example.com>
2023-01-11 21:02:48 +0000

    automation test

--- .space.kts
+++ .space.kts
@@ -5,22 +5,13 @@
 */
 
 job("Run shell script") {
-   container(displayName = "Run Test", image = "oberd/php-8.0-apache") {
-      shellScript {
-         interpreter = "/bin/bash"
-         content = """
-            export TZ=Europe/Paris
-            apt-get update -yqq
-            DEBIAN_FRONTEND=noninteractive apt-get install git libcurl4-gnutls-dev libicu-dev libmcrypt-dev libvpx-dev libjpeg-dev libpng-dev libxpm-dev zlib1g-dev libfreetype6-dev libxml2-dev libexpat1-dev libbz2-dev libgmp3-dev libldap2-dev unixodbc-dev libpq-dev libsqlite3-dev libaspell-dev libsnmp-dev libpcre3-dev libtidy-dev -yqq
-            docker-php-ext-install mbstring mcrypt pdo_mysql curl json intl gd xml zip bz2 opcache
-            
-            apt-get install curl -yqq
-            curl -sS https://getcomposer.org/installer | php
-            php composer.phar install -n
-
-            mkdir logs/ && chmod 777 logs/
-            ./vendor/bin/phpunit
-         """
-      }
-   }
+    container(displayName = "Show dir contents", image = "ubuntu") {
+        shellScript {
+            interpreter = "/bin/bash"
+            content = """
+                echo Working dir contents
+                ls /mnt/space/work
+            """
+        }
+    }
 }
